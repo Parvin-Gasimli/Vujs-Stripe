d726fa56ff82411d97d2b63b429989a4611af9ab
Parvin-Gasimli <parvinzga@code.edu.az>
2023-09-15 16:02:19 +0400

    Detail Page

--- ecommerce/components/ProductsComponent.vue
+++ ecommerce/components/ProductsComponent.vue
@@ -4,14 +4,37 @@
     cursor-pointer max-w-[400px] w-full
     ">
         <NuxtLink :to="`/item/${product.id}`">
-            <img class="rounded-t" :src="product.url" />
+            <img class="rounded-t w-full h-[200px]" :src="product.url" />
         </NuxtLink>
         <div id="ProductDetails">
             <span class="flex justify-between items-center  gap-3 px-1 pt-1">
                 <span class="text-[#FF6674] font-semibold">${{ PriceComputed }}</span>
                 <span class="text-gray-500 text-sm text-light line-through">${{ oldPriceComputed }}</span>
-
             </span>
+            <span class="px-1 my-2  text-[#FF6674] text-xs font-semibold">
+                extra 10% off
+            </span>
+            <div class="flex items-center gap-1 px-1 ">
+                <span class="px-[10px] py-[4px] rounded-sm text-[9px]  my-2  text-white bg-[#FD374F] text-xs font-semibold">
+                    Welcome Deal
+                </span>
+                <span
+                    class="px-[10px] py-[4px] rounded-sm text-[10px]  my-2  text-[#FFF] bg-[#000] border-[#000] text-xs font-semibold">
+                    Welcome Deal
+                </span>
+            </div>
+            <p class="flex items-center px-1 pt-0.5 text-xs text-[#252525]">
+                5.000+sold
+                <Icon name="*" color="#757575" class="ml-1.5" />4.7
+            </p>
+            <p class="px-1 pt-0.5 text-xs text-[#252525]">
+                {{ product.title.substring(0, 60) }} </p>
+            <p class="px-1 pb-1">
+                <span class="text-[#009A66] text-xs font-semibold">
+                    Free Shipping
+                </span>
+
+            </p>
         </div>
 
     </div>
---
+++ ecommerce/pages/item/[id].vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+            <div class="md:flex gap-4 justify-center items-center mr-auto w-full">
+                <div class="md:w-[100%] mr-auto">
+                    <img class="rounded-lg object-fit detailimg" src="https://picsum.photos/200/300" />
+
+                    <div v-if="images[0] !== ''" class="flex items-center gap-[10px] justify-center mt-2">
+                        <div v-for="image in images">
+                            <img @mouseover="currentImage = image" @click="currentImage = image" height="300" width="300"
+                                class="rounded-lg object-fit w-full -full" :src="image"
+                                :class="currentImage === image ? 'border-2 border-blue-500' : ''" />
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+let currentImage = ref(null)
+
+onMounted(() => {
+    watchEffect(() => {
+        currentImage.value = "https://picsum.photos/id/77/200/300"
+        images.value[0] = "https://picsum.photos/id/77/200/300"
+    })
+})
+const images = ref([
+    "",
+    "https://picsum.photos/id/2/200/300",
+    "https://picsum.photos/id/12/200/300",
+    "https://picsum.photos/id/33/200/300",
+    "https://picsum.photos/id/44/200/300",
+    "https://picsum.photos/id/56/200/300",
+])
+
+</script>
+
+<style lang="scss" scoped>
+.detailimg {
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+}
+</style>
